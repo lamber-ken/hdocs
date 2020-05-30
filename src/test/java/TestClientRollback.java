5dd3a823e1908d8a265e804d8ed00006df9faf1d
lamber-ken <xl38154@qq.com>
2020-05-30 23:19:28 +0800

    test12

--- src/test/java/TestClientRollback.java
+++ src/test/java/TestClientRollback.java
@@ -6,7 +6,7 @@ public class TestClientRollback {
     @Test
     public void testSavepointAndRollback() throws Exception {
 
-        // test 12
+        // test 1111
 
         System.out.println(1 / 0);
     }
